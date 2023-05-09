eb47aa159c2030ba342931fab7b1b2ef5d662666
Jordi Julià <jordijs6@gmail.com>
2023-05-09 10:08:29 +0200

    Adapt tailwindcss customizations to react

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}',],
   theme: {
+    colors: {
+      "completed": "#bedfc8",
+      "building": "#f8e9b9",
+      "soon": "#f7cbc4",
+      "projects-title": "#1c1c1e",
+      "grey-it": "#7e7e7e",
+    },
     extend: {},
   },
   plugins: [require("daisyui")]
