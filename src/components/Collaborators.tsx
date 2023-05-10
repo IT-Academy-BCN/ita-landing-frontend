dc7e72d7852bb495b685db96217652b88530c5ce
Joël Arana <joel.aranag@gmail.com>
2023-05-10 13:09:06 +0200

    tailwind button classes now in variables (#35)

--- src/components/Collaborators.tsx
+++ src/components/Collaborators.tsx
@@ -6,7 +6,9 @@ function Collaborators() {
     const [active, setActive] = useState("AngularCard");
     const [selectedItem, setSelectedItem] = useState(1);
     const VT = "Ver Todos >";
-
+    const btnActive = "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white";
+    const btnInactive = "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white";
+ 
     return (
         <section className="my-10">
         <div className="flex justify-center">
@@ -15,18 +17,18 @@ function Collaborators() {
         <div className="flex justify-center my-5">
         <div className="flex justify-center w-[726px]">
             <div className="flex navbar bg-base-100 drop-shadow-md justify-between rounded-full">
-                {/* <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" onClick={() => setActive("AngularCard")} >Angular</a> */}
-                <a className={ selectedItem === 1 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                
+                <a className={ selectedItem === 1 ? btnActive : btnInactive }
                                 onClick={() => {setActive("AngularCard"); setSelectedItem(1);}} >Angular</a>
-                <a className={ selectedItem === 2 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                <a className={ selectedItem === 2 ? btnActive : btnInactive }
                                 onClick={() => {setActive("ReactCard"); setSelectedItem(2);}} >React</a>
-                <a className={ selectedItem === 3 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                <a className={ selectedItem === 3 ? btnActive : btnInactive }
                                 onClick={() => {setActive("PHPCard"); setSelectedItem(3);}} >PHP</a>
-                <a className={ selectedItem === 4 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                <a className={ selectedItem === 4 ? btnActive : btnInactive }
                                 onClick={() => {setActive("NodeCard"); setSelectedItem(4);}} >Node</a>
-                <a className={ selectedItem === 5 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                <a className={ selectedItem === 5 ? btnActive : btnInactive }
                                 onClick={() => {setActive("JavaCard"); setSelectedItem(5);}} >Java</a>
-                <a className={ selectedItem === 6 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                <a className={ selectedItem === 6 ? btnActive : btnInactive }
                                 onClick={() => {setActive("DataScienceCard"); setSelectedItem(6);}} >Data Science</a>
             </div>
         </div>
