876d665c19abd8b3264b7b087c37129d26ce9df4
Joël Arana <joel.aranag@gmail.com>
2023-05-10 12:24:12 +0200

    Added footer to App tsx (#34)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Footer from "./components/footer"
+import Footer from "./components/Footer";
 import ExampleComponent from "./components/ExampleComponent";
 
 function App() {
