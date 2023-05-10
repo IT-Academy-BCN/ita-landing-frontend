4e8d530ebdb353d172fbc80bae5182e80eca57d7
Joël Arana <joel.aranag@gmail.com>
2023-05-10 12:26:00 +0200

    Fixed app.tsx merge and added header component (#34)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Footer from "./components/Footer";
 import ExampleComponent from "./components/ExampleComponent";
+import Header from "./components/HeaderComponent";
 
 function App() {
   return (
     <>
+      <Header />
       <ExampleComponent />
       <Footer />
     </>
