d421cebd51c3185c239fc1d98a6eabf8084a6f26
Joël Arana <joel.aranag@gmail.com>
2023-05-10 12:53:35 +0200

    Added collaborators section, can switch cards (#35)

--- src/components/Collaborators.tsx
+++ src/components/Collaborators.tsx
@@ -4,6 +4,7 @@ import Card from "./Card";
 
 function Collaborators() {
     const [active, setActive] = useState("AngularCard");
+    const [selectedItem, setSelectedItem] = useState(1);
     const VT = "Ver Todos >";
 
     return (
@@ -14,12 +15,19 @@ function Collaborators() {
         <div className="flex justify-center my-5">
         <div className="flex justify-center w-[726px]">
             <div className="flex navbar bg-base-100 drop-shadow-md justify-between rounded-full">
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" onClick={() => setActive("AngularCard")} >Angular</a>
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]" onClick={() => setActive("ReactCard")} >React</a>
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]" onClick={() => setActive("PHPCard")} >PHP</a>
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]" onClick={() => setActive("NodeCard")} >Node</a>
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]" onClick={() => setActive("JavaCard")} >Java</a>
-                <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E]" onClick={() => setActive("DataScienceCard")} >Data Science</a>
+                {/* <a className="btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" onClick={() => setActive("AngularCard")} >Angular</a> */}
+                <a className={ selectedItem === 1 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("AngularCard"); setSelectedItem(1);}} >Angular</a>
+                <a className={ selectedItem === 2 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("ReactCard"); setSelectedItem(2);}} >React</a>
+                <a className={ selectedItem === 3 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("PHPCard"); setSelectedItem(3);}} >PHP</a>
+                <a className={ selectedItem === 4 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("NodeCard"); setSelectedItem(4);}} >Node</a>
+                <a className={ selectedItem === 5 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("JavaCard"); setSelectedItem(5);}} >Java</a>
+                <a className={ selectedItem === 6 ? "btn btn-ghost text-sm normal-case rounded-3xl bg-[#025E7E] text-white" : "btn btn-ghost text-sm normal-case rounded-3xl active:bg-[#025E7E] active:text-white" }
+                                onClick={() => {setActive("DataScienceCard"); setSelectedItem(6);}} >Data Science</a>
             </div>
         </div>
         </div>
