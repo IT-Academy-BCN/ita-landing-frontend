d6648435c6bd3778cbcb78a2c0faee88e07eea68
Lauddar <lauragarciarod@gmail.com>
2023-05-10 12:05:37 +0200

    adapt responsive

--- src/components/teamSection/CardComponent.tsx
+++ src/components/teamSection/CardComponent.tsx
@@ -13,37 +13,39 @@ type CardTwoLogosProps = {
 const CardComponent = ({ pos, imgSrc1, imgSrc2, title, description, buttonText }: CardTwoLogosProps) => {
     let paragraphs = description.split('\n');
     return (
-        <div className="relative mt-4">
-            <svg width="360" height="410" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <div className="relative w-full sm:w-1/2 lg:w-1/4 p-3">
+            <svg className="w-full h-full" viewBox="0 0 268 329" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M15.1637 43.8887C5.60393 43.8887 0.000167875 51.1484 0.00016269 59.0682C0.76933 85.6874 1.41757e-06 124.736 0 217.133V308.211C2.7589e-05 321.08 4.94464 329 11.208 329H253.825C261.077 329 268 322.73 268 315.47V14.8495C268 4.61986 262.066 0 252.836 0H58.6766C51.4245 0 45.8205 5.27984 45.8205 12.8696V31.349C45.8205 38.9388 39.887 43.8887 32.6348 43.8887H15.1637Z" fill="#F0F0F0" />
             </svg>
-            <div className="overlay absolute inset-0 flex flex-col w-full h-full justify-between">
-                <div className="p-6 pb-0 1/5">
-                    <p className="font-normal font-medium text-base leading-5 text-gray-600 text-left">/ {pos}</p>
+            <div className="overlay absolute inset-0 flex flex-col w-full h-full justify-between px-6 py-7">
+                <div className="pb-0 1/5">
+                    <p className="font-medium text-base text-sm text-gray-600 text-left">/ {pos}</p>
                 </div>
-                <div className="flex justify-center items-center relative">
+                <div className="flex justify-center items-center relative py-3">
                     {imgSrc2 ? (
                         <>
-                            <img className="absolute translate-x-12 translate-y-8" src={imgSrc2} alt="Logo"></img>
-                            <img className="absolute -translate-x-12 -translate-y-8" src={imgSrc1} alt="Logo"></img>
+                            <img className="absolute" src={imgSrc2} alt="Logo"></img>
+                            <img className="absolute" src={imgSrc1} alt="Logo"></img>
                         </>
                     ) : (
                         <img src={imgSrc1} alt="Logo"></img>
                     )}
                 </div>
-                <div className="flex justify-center items-center p-3 h-1/5">
-                    <p className="text-center font-medium text-2xl text-black">{title}</p>
+                <div className="flex justify-center items-center h-1/6">
+                    <p className="text-center font-semibold text-2xl text-black">{title}</p>
                 </div>
-                <a href="#" className="overlay absolute inset-0 flex flex-col m-[45px] mt-[75px] h-4/5 overflow-hidden opacity-0 text-left hover:opacity-100" style={{ backgroundColor: "#F0F0F0" }}>
-                    {paragraphs.map((paragraphs, index) => (
-                        <p key={index} className="font-medium text-xl text-black overflow-hidden mb-3">{paragraphs}</p>
-                    ))}
-                    <div className="flex items-center mt-5">
-                        <div className="relative 1/5">
+                <a href="#" className="absolute inset-0 flex flex-col overflow-hidden m-6 mt-16 p-5 pt-2 text-left opacity-0 hover:opacity-100" style={{ backgroundColor: "#F0F0F0" }}>
+                    <div className="h-5/6 overflow-hidden">
+                        {paragraphs.map((paragraphs, index) => (
+                            <p key={index} className="font-medium text-sm text-black mb-3">{paragraphs}</p>
+                        ))}
+                    </div>
+                    <div className="flex items-center h-1/6">
+                        <div className="relative">
                             <img className="" src={ellipse} alt=""></img>
                             <img className="absolute top-1 left-1.5" src={vector} alt="" />
                         </div>
-                        <div className="ml-3 text-black font-bold text-xl">
+                        <div className="ml-3 text-black text-sm font-bold text-xl">
                             <p className="align-center">
                                 {buttonText}
                             </p>
--- src/components/teamSection/TeamSectionComponent.tsx
+++ src/components/teamSection/TeamSectionComponent.tsx
@@ -7,14 +7,14 @@ import nodejsLogo from '../../img/nodejs.png';
 import CardComponent from './CardComponent';
 
 const TeamsectionComponent = () => (
-    <div className="p-14">
+    <div>
         <div className="w-full flex flex-col items-center justify-center my-12 space-y-8">
             <h1 className="font-poppins font-bold text-5xl leading-58 text-gray-800">Equipos de trabajo</h1>
             <h3 className="font-poppins font-medium text-base text-gray-500">Los proyectos son realizados por
                 equipos que combinan las siguientes tecnologías</h3>
         </div>
 
-        <div className="flex w-full items-center justify-center md:flex-row flex-col space-y-4">
+        <div className="flex flex-wrap items-center justify-center">
             <CardComponent pos={'01'} imgSrc1={reactLogo} imgSrc2={nodejsLogo} title={'React & Node'} description={'Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.\nVelitbus vulputate neque lacinia faucibus blandit diam parturient luctus.'} buttonText={'Ver más'} />
             <CardComponent pos={'02'} imgSrc1={angularLogo} imgSrc2={javaLogo} title={'Angular & Java'} description={'Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.\nVelitbus vulputate neque lacinia faucibus blandit diam parturient luctus.'} buttonText={'Ver más'} />
             <CardComponent pos={'03'} imgSrc1={phpLogo} title={'PHP'} description={'Lorem ipsum dolor sit amet consectetur adipiscing elit nullam pellentesque dignissim interdum feugiat tortor ut.\nVelitbus vulputate neque lacinia faucibus blandit diam parturient luctus.'} buttonText={'Ver más'} />
