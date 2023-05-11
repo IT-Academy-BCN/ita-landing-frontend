1ec2092db4fb2c1f7aef060898293c46dd395abc
Joël Arana <joel.aranag@gmail.com>
2023-05-11 12:45:11 +0200

    Added normativa Modal (#34)

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import "./App.css";
 import TeamSectionComponent from "./components/teamSection/TeamSectionComponent"
 import SloganComponent from "./components/SloganComponent"
 import HeaderComponent from "./components/HeaderComponent"
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
       <HeaderComponent />
       <SloganComponent />
       <TeamSectionComponent />
+      <Footer></Footer>
     </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,4 @@
+import NormativaModal from "./Modals/NormativaModal";
 
 const Footer = () => (
   <footer className="bg-[#BA007C] w-screen">
@@ -5,20 +6,22 @@ const Footer = () => (
     <div>
       <div className="flex justify-center items-center">
         <img
-          src="../../public/images/ITA_Logo.png"
+          src="../../src/images/ITA_Logo.png"
           className="h-10"
           alt="IT Academy Footer Logo"
         />
       </div>
       <div className="flex justify-center mt-10 font-semibold">
-        <a className="mx-5 text-white" href="https://www.barcelonactiva.cat/es/inicio">Barcelona Activa</a>
-        <a className="mx-5 text-white" href="https://www.barcelonactiva.cat/es/itacademy">IT Academy</a>
-        <a className="mx-5 text-white" href="">¿Cómo colaborar?</a>
+        <a className="mx-5 text-white hover:text-purple-600" href="https://www.barcelonactiva.cat/es/inicio">Barcelona Activa</a>
+        <a className="mx-5 text-white hover:text-purple-600" href="https://www.barcelonactiva.cat/es/itacademy">IT Academy</a>
+        <a className="mx-5 text-white hover:text-purple-600" href="">¿Cómo colaborar?</a>
+        <label htmlFor="NormativaModal" className="mx-5 text-white hover:text-purple-600 hover:cursor-pointer">Normativa</label>
+        <NormativaModal />
       </div>
       <div className="flex justify-center mt-20 font-medium pb-3">
-        <a className="mx-7 text-white" href="">Términos</a>
-        <a className="mx-7 text-white" href="">Privacidad</a>
-        <a className="mx-7 text-white" href="">Cookies</a>
+        <a className="mx-7 text-white hover:text-purple-600" href="">Términos</a>
+        <a className="mx-7 text-white hover:text-purple-600" href="">Privacidad</a>
+        <a className="mx-7 text-white hover:text-purple-600" href="">Cookies</a>
       </div>
     </div>
   </div>
