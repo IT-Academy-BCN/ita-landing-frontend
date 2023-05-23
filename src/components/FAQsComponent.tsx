65ca9e36eafccd3f18f59186411113616424f9a8
Rodri Saiz <rodrisaiz@icloud.com>
2023-05-23 12:42:48 +0200

    Correction of shadows, addition of json server with dummy data and implementation of logic to read this data

--- src/components/FAQsComponent.tsx
+++ src/components/FAQsComponent.tsx
@@ -1,70 +1,35 @@
-function FAQs() { 
-    
-    return(
-        
-        <> 
-        
-         {/*FAQs title area*/}
-         <div className="grid grid-cols-6 grid-rows-2">
-              <h2 className="font-black text-[35px] font-poppins  text-center col-span-6">
-                Preguntas frecuentes
-              </h2>
-        </div>
-    
-        {/* FAQs*/}
-        <div className="collapse  rounded-md pb-2">
-            <input type="checkbox" className="peer" />
-            <div className="collapse-title absolute  rounded-b-md bg-[#FFFF] text-left text-[#000000] text-[16px] font-poppins font-bold peer-checked:bg-[#BA007C] peer-checked:rounded-b-[0px] peer-checked:text-secondary-content text-[16px] font-poppins font-bold ">
-                ¿Tengo que acabar la especialización para colaborar en un proyecto?
-            </div>
-            <div className="collapse-content rounded-b-md bg-[#FFFF]">
-                <p className= "text-left text-[#000000]  text-[16px] font-poppins font-medium ml-[36px] mr-[90px] mt-[34px] mb-[23px]"> 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vehicula scelerisque. Donec luctus faucibus rutrum. Nulla sed nisl vitae ipsum rhoncus vestibulum et eget erat. 
-                    Duis non nunc ante. Nunc sagittis pellentesque massa, 
-                    a placerat sem tempor non. Nunc cursus libero at luctus dictum. Integer convallis consequat vulputate.
-                    Vestibulum eget libero id massa iaculis consequat at eu tellus. 
-                    
-                    Maecenas quis blandit ligula.</p>
-            </div>
-        </div>
-    
-        <div className="collapse  rounded-md pb-2">
-            <input type="checkbox" className="peer" />
-            <div className="collapse-title absolute  rounded-b-md bg-[#FFFF] text-left text-[#000000] text-[16px] font-poppins font-bold peer-checked:bg-[#BA007C] peer-checked:rounded-b-[0px] peer-checked:text-secondary-content text-[16px] font-poppins font-bold ">
-                ¿Tengo que acabar la especialización para colaborar en un proyecto?
-            </div>
-            <div className="collapse-content rounded-b-md bg-[#FFFF]">
-                <p className= "text-left text-[#000000]  text-[16px] font-poppins font-medium ml-[36px] mr-[90px] mt-[34px] mb-[23px]"> 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vehicula scelerisque. Donec luctus faucibus rutrum. Nulla sed nisl vitae ipsum rhoncus vestibulum et eget erat. 
-                    Duis non nunc ante. Nunc sagittis pellentesque massa, 
-                    a placerat sem tempor non. Nunc cursus libero at luctus dictum. Integer convallis consequat vulputate.
-                    Vestibulum eget libero id massa iaculis consequat at eu tellus. 
-                    
-                    Maecenas quis blandit ligula.</p>
-            </div>
-        </div>
-    
-        <div className="collapse  rounded-md pb-2">
-            <input type="checkbox" className="peer" />
-            <div className="collapse-title absolute  rounded-b-md bg-[#FFFF] text-left text-[#000000] text-[16px] font-poppins font-bold peer-checked:bg-[#BA007C] peer-checked:rounded-b-[0px] peer-checked:text-secondary-content text-[16px] font-poppins font-bold ">
-                ¿Tengo que acabar la especialización para colaborar en un proyecto?
-            </div>
-            <div className="collapse-content rounded-b-md bg-[#FFFF]">
-                <p className= "text-left text-[#000000]  text-[16px] font-poppins font-medium ml-[36px] mr-[90px] mt-[34px] mb-[23px]"> 
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean gravida vehicula scelerisque. Donec luctus faucibus rutrum. Nulla sed nisl vitae ipsum rhoncus vestibulum et eget erat. 
-                    Duis non nunc ante. Nunc sagittis pellentesque massa, 
-                    a placerat sem tempor non. Nunc cursus libero at luctus dictum. Integer convallis consequat vulputate.
-                    Vestibulum eget libero id massa iaculis consequat at eu tellus. 
-                    
-                    Maecenas quis blandit ligula.</p>
-            </div>
-        </div>
-    
-        
-        
-    
-        </>
-    );
+import  {useState} from "react";
+import data from "../mock-FAQS-data.json";
 
-}  
-  export default FAQs; 
+
+const FAQs = () => {
+
+    const [ MultipleFaqs, setMultipleFaqs] = useState(data);
+
+    return (
+        <div>
+            <div className="grid grid-cols-6 grid-rows-2">
+                <h2 className="font-black text-[35px] font-poppins  text-center col-span-6">
+                Preguntas frecuentes
+                </h2>
+            </div>
+            {MultipleFaqs.map((faq)=>
+            <div className="collapse  rounded-md pb-2 shadow-[0_0px_2px_12px_rgba(0,0,0,0.1)]">
+                <input type="checkbox" className="peer" />
+                <div className="collapse-title absolute  rounded-b-md bg-[#FFFF] text-left text-[#000000] text-[16px] font-poppins font-bold peer-checked:bg-[#BA007C] peer-checked:rounded-b-[0px] peer-checked:text-secondary-content text-[16px] font-poppins font-bold ">
+                {faq.title}
+                </div>
+                <div className="collapse-content rounded-b-md bg-[#FFFF]">
+                    <p className= "text-left text-[#000000]  text-[16px] font-poppins font-medium ml-[36px] mr-[90px] mt-[34px] mb-[23px]"> 
+                    {faq.answer}
+                    </p>
+                </div>
+            </div>
+            )}
+        </div>
+
+    )
+
+
+}
+export default FAQs; 
