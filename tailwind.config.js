cb446dc3fa44be9717b9046fb5c611886a46e0e8
vicenlu <bcnluisvi@gmail.com>
2023-05-30 17:31:16 +0200

    workaround tailwind css classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}',],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      "completed": "#bedfc8",
-      "building": "#f8e9b9",
-      "soon": "#f7cbc4",
+      completed: "#bedfc8",
+      building: "#f8e9b9",
+      soon: "#f7cbc4",
       "projects-title": "#1c1c1e",
       "grey-it": "#7e7e7e",
     },
     extend: {},
   },
-  plugins: [require("daisyui")]
-}
+  plugins: [require("daisyui")],
+};
