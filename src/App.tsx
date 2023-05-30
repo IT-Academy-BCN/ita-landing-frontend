3f324a909ef7284ae8d9d86ed35523f0e46acabc
Rodri Saiz <rodrisaiz@icloud.com>
2023-05-30 14:15:25 +0200

    fix import header app

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 
 import TeamSectionComponent from "./components/teamSection/TeamSectionComponent"
 import SloganComponent from "./components/SloganComponent"
-import HeaderComponent from "./components/HeaderSection/HeaderComponent"
+import HeaderComponent from "./components/HeaderComponent"
 import Footer from "./components/FooterComponent"
 import ProjectsComponent from "./components/ProjectsComponent"
 import Collaborators from "./components/CollaboratorsSection/Collaborators"
