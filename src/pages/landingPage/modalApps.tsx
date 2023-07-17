5b4f374c4babdc67ff10a64ba64ce49b4b86fb6a
javier <javiergarciaurruchi@gmail.com>
2023-07-17 14:24:49 +0200

    conflicts solved checks

--- src/pages/landingPage/modalApps.tsx
+++ src/pages/landingPage/modalApps.tsx
@@ -1,5 +1,5 @@
  
-import {useEffect,useState} from 'react'
+import {useEffect} from 'react'
 import { useDispatch,useSelector } from 'react-redux'
 import { RootState } from '../../store/store';
 import { deleteApiApps } from '../../store/reducers/appsCall/appsCallApiFunctionality';
