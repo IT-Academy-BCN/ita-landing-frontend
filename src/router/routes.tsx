7113935f91c7e09cb8510671a8e26eb41eb986e7
javier <javiergarciaurruchi@gmail.com>
2023-07-17 17:04:38 +0200

    solved conflict store

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes} from "react-router-dom"
 import LandingPage from "../pages/landingPage/LandingPage"
 import BackOffice from "../pages/backOffice/BackOffice"
-import ModalApps from "../pages/landingPage/modalApps"
 import ProtectedRoute from "./protectedRoutes"
 
 function Router() {
@@ -11,7 +10,6 @@ function Router() {
             <Route path="/" element={<LandingPage/>} />
             <Route element={<ProtectedRoute/>}>
                 <Route path="/backoffice" element={<BackOffice/>} />
-                <Route path="/backoffice/:id" element={<ModalApps/>} />
             </Route>
         </Routes>
         </>
