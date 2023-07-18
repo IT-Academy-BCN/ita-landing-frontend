8c741c7c501b8962a7bad5f031c8773b357477ff
AlbertLnz <albert2000_lanza@gmail.com>
2023-07-18 14:57:36 +0200

    Fix useRef declared but value never read

--- src/pages/landingPage/FAQsComponent.tsx
+++ src/pages/landingPage/FAQsComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect , useState, useRef  } from "react";
+import { useEffect , useState } from "react";
 import { useSelector,useDispatch } from "react-redux";
 import { RootState } from "../../store/store";
 import FaqsModified from "./FaqsModified";
