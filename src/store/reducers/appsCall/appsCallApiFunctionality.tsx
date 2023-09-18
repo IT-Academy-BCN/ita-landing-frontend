8d326d20ab9de7d46d27740d37909a0f59b64dd0
LUIS VICENTE <bcnluisvi@gmail.com>
2023-09-18 16:37:09 +0200

    test CORS

--- src/store/reducers/appsCall/appsCallApiFunctionality.tsx
+++ src/store/reducers/appsCall/appsCallApiFunctionality.tsx
@@ -1,112 +1,126 @@
-import { createSlice } from '@reduxjs/toolkit'
-import axios from 'axios' 
-import { Dispatch } from '@reduxjs/toolkit'
-import { faqContent } from '../../../interfaces/interfaces'
+import { createSlice, Dispatch } from "@reduxjs/toolkit";
+import axios from "axios";
+import { faqContent } from "../../../interfaces/interfaces";
 const initialState = {
-  apps:[],
-  appsInfo:[],
-  gitURL:'',
-  repoURL:'',
-  stateProject:'',
-  loadingApps:false 
-} 
+  apps: [],
+  appsInfo: [],
+  gitURL: "",
+  repoURL: "",
+  stateProject: "",
+  loadingApps: false,
+};
 export const apiSlice = createSlice({
-  name: 'appsCallApiFunctionality',
+  name: "appsCallApiFunctionality",
   initialState,
   reducers: {
-    setApps:(state,action)=>{
-        state.apps = action.payload;
-    },setAppsInfo:(state,actions)=>{
-      state.appsInfo= actions.payload
-    },setLoadingApps:(state,actions)=>{
-      state.loadingApps=actions.payload
-    }
+    setApps: (state, action) => {
+      state.apps = action.payload;
+    },
+    setAppsInfo: (state, actions) => {
+      state.appsInfo = actions.payload;
+    },
+    setLoadingApps: (state, actions) => {
+      state.loadingApps = actions.payload;
+    },
   },
-})
+});
 
-export const { setApps,setAppsInfo,setLoadingApps } = apiSlice.actions;
+export const { setApps, setAppsInfo, setLoadingApps } = apiSlice.actions;
 
-  //llamada api a apps//
+//llamada api a apps//
 
- export const apiCallApps =async(dispatch:Dispatch)=>{
-    try {
-        const faqsCalled = await axios.get('http://87.106.229.119/api/apps');
-    dispatch(setApps(faqsCalled.data))
-        
-    } catch (error) {
-        console.log(error)
-    }
-    
+export const apiCallApps = async (dispatch: Dispatch) => {
+  try {
+    const config = {
+      url: "http://87.106.229.119/api/apps",
+      method: "GET",
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    };
+    const faqsCalled = await axios(config);
+    dispatch(setApps(faqsCalled.data));
+  } catch (error) {
+    console.log(error);
   }
-  //llamada api Apps ID//
-  export const apiCallAppsInfo =async(dispatch:Dispatch,id:number,acces_token:string)=>{
-    try {
-      dispatch(setLoadingApps(true));
+};
+//llamada api Apps ID//
+export const apiCallAppsInfo = async (
+  dispatch: Dispatch,
+  id: number,
+  acces_token: string
+) => {
+  try {
+    dispatch(setLoadingApps(true));
 
-        const faqsCalled = await axios.get(`http://87.106.229.119/api/apps/${id}`,{
-          headers:{
-            Authorization: `Bearer ${acces_token}`
-          }
-        });
-        await dispatch(setAppsInfo(faqsCalled.data))
-        console.log(faqsCalled.data,`http://87.106.229.119/api/apps/${id}`)
+    const faqsCalled = await axios.get(`http://87.106.229.119/api/apps/${id}`, {
+      headers: {
+        Authorization: `Bearer ${acces_token}`,
+      },
+    });
+    await dispatch(setAppsInfo(faqsCalled.data));
+    console.log(faqsCalled.data, `http://87.106.229.119/api/apps/${id}`);
 
-        dispatch(setLoadingApps(false))
-        
-    } catch (error) {
-        console.log(error,`http://87.106.229.119/api/apps/${id}`)
-    }
-    
+    dispatch(setLoadingApps(false));
+  } catch (error) {
+    console.log(error, `http://87.106.229.119/api/apps/${id}`);
   }
-  //Post //
- export const postApiApps =async(appsContent:faqContent,acces_token:string, dispatch:Dispatch)=>{
-    try{
-        
-     await axios.post('http://87.106.229.119/api/apps',appsContent,{
-      headers:{
-        Authorization:`Bearer ${acces_token}`
-      }
-     });  
+};
+//Post //
+export const postApiApps = async (
+  appsContent: faqContent,
+  acces_token: string,
+  dispatch: Dispatch
+) => {
+  try {
+    await axios.post("http://87.106.229.119/api/apps", appsContent, {
+      headers: {
+        Authorization: `Bearer ${acces_token}`,
+      },
+    });
     await apiCallApps(dispatch);
-    }catch(error){
-      console.log(error)
-    }
-   
-  };
-  //delete //
+  } catch (error) {
+    console.log(error);
+  }
+};
+//delete //
 
-  export const deleteApiApps = async (appsID:number, acces_token:string,dispatch:Dispatch) => {
-    try {
-      const url = `http://87.106.229.119/api/apps/${appsID}`;
-      await axios.delete(url,{
-        headers:{
-          Authorization: `Bearer ${acces_token}`
-        }
-      });
-      
-      await apiCallApps(dispatch);
-         
-       
-    } catch (error) {
-      console.error("Error al eliminar el FAQ:", error);
-    }
-  };
+export const deleteApiApps = async (
+  appsID: number,
+  acces_token: string,
+  dispatch: Dispatch
+) => {
+  try {
+    const url = `http://87.106.229.119/api/apps/${appsID}`;
+    await axios.delete(url, {
+      headers: {
+        Authorization: `Bearer ${acces_token}`,
+      },
+    });
+
+    await apiCallApps(dispatch);
+  } catch (error) {
+    console.error("Error al eliminar el FAQ:", error);
+  }
+};
 //put//
 
-export const putApiApps =async(faqContent:any,acces_token:string, dispatch:any, id:number)=>{
-  try{
-     
-   await axios.put('http://87.106.229.119/api/apps/'+id,faqContent,{
-    headers:{
-      Authorization:`Bearer ${acces_token}`
-    }
-   });  
-  await apiCallApps(dispatch);
-  }catch(error){
-    console.log(error)
+export const putApiApps = async (
+  faqContent: any,
+  acces_token: string,
+  dispatch: any,
+  id: number
+) => {
+  try {
+    await axios.put("http://87.106.229.119/api/apps/" + id, faqContent, {
+      headers: {
+        Authorization: `Bearer ${acces_token}`,
+      },
+    });
+    await apiCallApps(dispatch);
+  } catch (error) {
+    console.log(error);
   }
- 
 };
 
-
-export default apiSlice.reducer
+export default apiSlice.reducer;
