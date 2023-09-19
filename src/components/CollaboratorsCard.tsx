be5456209227357547396d5e7813d5e92d853645
javier <javiergarciaurruchi@gmail.com>
2023-09-19 22:32:16 +0200

    version 1 collaborators component

--- src/components/CollaboratorsCard.tsx
+++ src/components/CollaboratorsCard.tsx
@@ -1,28 +1,39 @@
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector, } from "react-redux";
 import { getCollaboratorsData } from "../store/reducers/CollaboratorsCall/getCollaboratorsData";
-import { useEffect } from "react";
-
+import { useEffect,useState } from "react";
+import { collaborator } from "../interfaces/interfaces";
 
 const CollaboratorsCard = ({title}: {title: string}) => {
+
+const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+
+//Variables Redux//
  const dispatch =  useDispatch();
- const {collaborators} = useSelector((state:any)=> state.getCollaboratorsFunctionality)
+
+ const {collaborators} = useSelector((state:any)=> state.getCollaboratorsFunctionality);
+
+//Inicialization of the api//
    useEffect(()=>{
+    
     dispatch(getCollaboratorsData() as any);
-   
    },[]);
+
+
 
    console.log(collaborators)
     return (
-        <div className="card">
-            <p>{title}</p>
+    <div className="grid grid-cols-1 gap-y-5    md:w-11/12 m-auto md:grid-cols-3 lg:grid-cols-3">
+        
             {
-                collaborators.map((a:any)=>{
+                collaborators.slice(window.innerWidth > 750 ? 0 : 0, window.innerWidth > 750 ? 15 : 4).map((a:collaborator, index:number)=>{
                     
-                    return <div> <p>{a.name}</p> <img src={a.photo} alt="" /> </div>
+                    return  <div className="w-2/3 m-auto"   key={index} > <a  className="flex items-center justify-center "href={a.url} target="_blank"  >  <img className="w-32 md:w-20 rounded-full" src={a.photo} alt={a.name+ ' photo'} /> <p className="ms-5  w-full text-start">{a.name}</p> </a> </div>
                 })
             }
         </div>
     )
 }
+
 
 export default CollaboratorsCard;
