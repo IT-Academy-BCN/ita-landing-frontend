5d3bfdd8327cfc4410414ca8fa7cf7e3937a3417
Darya Bochkovska <123471637+Dar-ya218@users.noreply.github.com>
2023-10-25 12:22:21 +0200

    popup done #81

--- src/components/PopupHowToCollaborate.tsx
+++ src/components/PopupHowToCollaborate.tsx
@@ -1,39 +1,36 @@
-import React, { useState } from 'react';
-import CardComponent from "./CardComponent";
-import ButtonCollaborate from './ButtonCollaborate';
+/* import React, { useState } from 'react'; */
+/* import ButtonCollaborate from './ButtonCollaborate'; */
 import folder from '../assets/img/new-folder-dynamic-color.png';
 import puzzle from '../assets/img/puzzle-dynamic-color.png';
 import thumb_up from '../assets/img/thumb-up-dynamic-color.png';
+import CardComponentPopupCollaborate from './CardComponentPopupCollaborate';
+import CardComponentPopupCollaboratePopupCollaborate from './CardComponentPopupCollaborate';
 
 const PopupHowToCollaborate: React.FC = () => {
-  const [isPopupOpenLocal, setisPopupOpenLocal] = useState(false);
+ /*  const [isPopupOpenLocal, setisPopupOpenLocal] = useState(false); */
 
   return (
-    <div className="font-black">
-      <p className="flex items-center justify-start">
-      <ButtonCollaborate onClick={() => setisPopupOpenLocal(true)} />
-      </p>
-
-      {isPopupOpenLocal && (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-80">
-          <div className="bg-white p-6 rounded-lg max-w-md">
-            <h2 className="text-xl font-semibold">¿Cómo coloborar en proyectos IT Academy?</h2>
+    <div className="font-black bg-white w-3/4 fixed z-50 top-10">
+      <h1 className='text-3xl text-black font-semibold'>¿Cómo coloborar en proyectos IT Academy?</h1>
+      
+      <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-80">
+     
             <div className="flex flex-wrap items-center justify-center container mx-auto"> 
-                <CardComponent 
+                <CardComponentPopupCollaboratePopupCollaborate
                 pos={"01"}
                 imgSrc1={folder}
                 title="Pregunta a tu mentor"
                 description="Resolvera todas tus dudas y te ayudara con setup del proyecto"
                 />
 
-                <CardComponent
+                <CardComponentPopupCollaborate
                 pos={"02"}
                 imgSrc1={puzzle}
                 title="Unete al equipo"
                 description="Tu mentor de proyecto te ayudará"
                 />
 
-                <CardComponent
+                <CardComponentPopupCollaborate
                 pos={"03"}
                 imgSrc1={thumb_up}
                 title="Entrega tu funcionalidades"
@@ -42,8 +39,6 @@ const PopupHowToCollaborate: React.FC = () => {
             </div>
           </div>
         </div>
-      )}
-    </div>
   );
 };
 
