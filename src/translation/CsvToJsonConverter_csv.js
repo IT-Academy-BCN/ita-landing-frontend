ad89eb70d11a8198613216f2f2a0ea46a5fccced
LUIS VICENTE <bcnluisvi@gmail.com>
2023-11-14 13:52:26 +0100

    translation script improved

--- src/translation/CsvToJsonConverter_csv.js
+++ src/translation/CsvToJsonConverter_csv.js
@@ -1,22 +1,45 @@
 import csv from "csvtojson";
-import fs from 'fs';
+import fs from "fs";
+const csvFilePath = `src/translation/files_csv/excel_traducciones.csv`;
+let json = { ca: {}, es: {}, en: {} };
+let languages = ["ca", "es", "en"];
 
-// SIN EXPRESIONES REGULARES --> para que funcione en JsonToCsvConverter.js
-export function csvToJsonConverter(lang) {
-    const csvFilePath = `src/translation/files_csv/${lang}.csv`;
+export function csvToJsonConverter() {
+  // Read csv file
+  csv()
+    .fromFile(csvFilePath)
+    .then((jsonArray) => {
+      // jsonArray -> array of csv rows
+      jsonArray.forEach((row) => {
+        // Locale with section
+        if (row.section) {
+          languages.forEach((lang) => {
+            if (!row[lang]) return;
+            if (!json[lang][row.section]) json[lang][row.section] = {};
+            json[lang][row.section][row.key] = row[lang];
+          });
+          // Locale without section
+        } else {
+          languages.forEach((lang) => {
+            if (!row[lang]) return;
+            json[lang][row.key] = row[lang];
+          });
+        }
+      });
 
-    csv()
-        .fromFile(csvFilePath)
-        .then((jsonArray) => {
-            const jsonData = jsonArray[0];
-            
-            fs.writeFile(`src/translation/languages/${lang}.json`, JSON.stringify(jsonData, null, 4), 'utf8', function (err) {
-                if (err) throw err;
-                console.log(`${lang}.json file updated!`);
-            });
-        });
+      // Create Json files
+      languages.forEach((lang) => {
+        fs.writeFile(
+          `src/translation/languages/${lang}.json`,
+          JSON.stringify(json[lang], null, 4),
+          "utf8",
+          function (err) {
+            if (err) throw err;
+            console.log(`${lang}.json file updated!`);
+          }
+        );
+      });
+    });
 }
 
-csvToJsonConverter('catala')
-csvToJsonConverter('english')
-csvToJsonConverter('espanol')
+csvToJsonConverter();
