13267a208fccc533a7c7a498b9d1913a295c766e
treee97 <luduve97@gmail.com>
2023-11-21 19:19:31 +0100

    modal not visible in lg

--- src/components/faqs/faqsAdminView/AdminButtons.tsx
+++ src/components/faqs/faqsAdminView/AdminButtons.tsx
@@ -1,16 +1,10 @@
-import { useState } from "react";
 import plusIcon from "../../../assets/img/plus.svg";
 import userIcon from "../../../assets/img/user.svg";
 import { useDispatch } from "react-redux";
 import { setIsLogged } from "../../../store/reducers/apiCall/apiPostRegisterLogin";
 
 export const AdminButtons = () => {
-  const [modalOpen, setModalOpen] = useState(false);
   const dispatch = useDispatch();
-
-  const handleClickUserButton = () => {
-    modalOpen === false ? setModalOpen(true) : setModalOpen(false);
-  };
 
   const handleLogout = () => {
     dispatch(setIsLogged(false));
@@ -22,16 +16,33 @@ export const AdminButtons = () => {
         <a className="flex items-center justify-center mr-1 w-12 h-10 rounded-lg bg-white">
           <img src={plusIcon} alt="plusIcon" className="w-5" />
         </a>
-        <button className="flex items-center justify-center ml-1 w-12 h-10 rounded-lg bg-white" onClick={handleClickUserButton}>
+
+        <button
+          className="flex items-center justify-center ml-1 w-12 h-10 rounded-lg bg-white cursor-pointer"
+          onClick={() => {
+            if (document) {
+              (
+                document.getElementById("my_modal_1") as HTMLFormElement
+              ).showModal();
+            }
+          }}
+        >
           <img src={userIcon} alt="userIcon" className="w-8" />
         </button>
-
-        {modalOpen && (
-          <button className="absolute z-50 rounded-lg font-bold bg-white border-2 border-gray-400 top-12 py-2 px-8 my-4 mx-6 mr-2 hover-bg-transparent" onClick={handleLogout}>
+        {/* The modal in question */}
+        <dialog id="my_modal_1" className="modal">
+          <button
+            className="modal-box absolute top-20 right-20 w-40 py-4 font-bold shadow text-center hover:bg-backOffice-main"
+            onClick={handleLogout}
+          >
             Logout
           </button>
-        )}
+          {/* This closes the modal if we click anywhere else. */}
+          <form method="dialog" className="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
       </div>
     </>
   );
-}
+};
