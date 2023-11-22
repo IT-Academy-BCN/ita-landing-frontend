0ee69d070736b886fbb4f5efc33307c7090fca2f
Mialomi <e.silvasm@gmail.com>
2023-11-22 18:57:11 +0100

    change h42px to h-11

--- src/components/apps/Apps.tsx
+++ src/components/apps/Apps.tsx
@@ -60,7 +60,7 @@ const Apps = () => {
               >
                 {/* Botones editar/eliminar superior */}
                     {window.location.pathname === "/backoffice" && (
-                        <div className="flex place-content-end gap-2 h-[42px]">
+                        <div className="flex gap-2 place-content-end h-11">
                             <button className="bg-white px-4 mt-3 text-sm text-[#808080] rounded-lg font-semibold" onClick={() => {window.my_modal_1?.showModal(); handleSendApiInfo(app.id); }}>{t("backofficePage.appsComponent.editButton.editButtonTitle")}</button>
                             <a className="flex mt-3 mr-4" onClick={() => deleteApiApps(app.id, acces_token, dispatch)} onKeyDown={(event) => { if (event.key === 'Enter') deleteApiApps(app.id, acces_token, dispatch); }}>
                                 <img src={trashIcon} alt="eliminar" className="w-10" />
@@ -68,7 +68,7 @@ const Apps = () => {
                         </div>
                     )}
                     {window.location.pathname === "/" && (
-                        <a href={app.github} className="flex place-content-end px-4 py-1 mt-4" target="_blank">
+                        <a href={app.github} className="flex px-4 py-1 mt-4 place-content-end" target="_blank">
                             <img src={githubLogo} alt="github_link" />
                         </a>
                     )}
